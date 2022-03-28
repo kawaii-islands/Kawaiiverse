7d86772e44ab88e7cbf7f10f6e9388c701086f97
nguyenHoang32 <nguyenhuuhoang2k15@gmail.com>
2022-03-28 10:29:33 +0700

    ui sell nft

--- src/pages/Profile/Store/SellItemNFT/index.js
+++ src/pages/Profile/Store/SellItemNFT/index.js
@@ -1,13 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "@mui/material";
+import axios from "axios";
 import cn from "classnames/bind";
 import styles from "./index.module.scss";
 import { Col, Row } from "antd";
-
+import { toast } from "react-toastify";
+import Item from "./Item";
 const cx = cn.bind(styles);
 
 const SellItemNFT = () => {
-  return (
+  const [list, setList] = useState([]);
+  const [rowItem, setRowItem] = useState(1);
+  const [listSell, setListSell] = useState([]);
+  useEffect(() => {
+    getListNft();
+  }, []);
+
+  const getListNft = async () => {
+    try {
+      const res = await axios.get('http://159.223.81.170:3000/v1/nft/0xD6eb653866F629e372151f6b5a12762D16E192f5');
+      if(res.status === 200){
+        setList(res.data.data);
+      }else{
+        toast.error("Cannot get list Nft")
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error)
+    }
+  }
+  const addItem = () => {
+    setRowItem(rowItem + 1);
+  }
+  const sellNft = () => {
+    console.log(listSell);
+  }
+  return ( 
     <div className={cx("table")}>
       <Row className={cx("table-header")}>
         <Col span={12} style={{ textAlign: "center" }}>
@@ -20,22 +48,17 @@ const SellItemNFT = () => {
         </Col>
       </Row>
       <div className={cx("table-body")}>
-        <Row className={cx("table-body-header")}>
-          <Col span={12} style={{ textAlign: "center" }}>
-            <input type="search" placeholder="Seach name, ID" className={cx("search-input")}/>
-          </Col>
-          <Col span={4}>------</Col>
-          <Col span={7}>
-            <div className={cx("quantity")}>
-              <div className={cx("quantity-btn", "quantity-minus")}>-</div>
-              <input value={20} />
-              <div className={cx("quantity-btn", "quantity-plus")}>+</div>
-            </div>
-          </Col>
-          <Col span={1}>
-            <input type="checkbox" />
-          </Col>
-        </Row>
+        {new Array(rowItem).fill().map(i => <Item list={list} listSell={listSell} setListSell={setListSell}/>)}
+      </div>
+      <div className={cx("wrapper-btn")}>
+        <Button 
+        className={cx("wrapper-btn--sell")}
+        onClick={sellNft}
+        >SELL NFT</Button>
+        <Button 
+        className={cx("wrapper-btn--add")}
+        onClick={addItem}
+        >ADD NFT</Button>
       </div>
     </div>
   );

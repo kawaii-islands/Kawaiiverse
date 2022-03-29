094584fa7fdcd747c0be28c28f73ace3406236db
lytrungkienorai <kien.lt@orai.io>
2022-03-29 11:36:59 +0700

    fix: navigation tab

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -36,7 +36,7 @@ const Profile = () => {
 	const [openFilterModal, setOpenFilterModal] = useState(false);
 	const [gameList, setGameList] = useState([]);
 	const [gameSelected, setGameSelected] = useState(KAWAII1155_ADDRESS);
-	const [activeTab, setActiveTab] = useState(0);
+	const [activeTab, setActiveTab] = useState(1);
 	const tabParam = useParams();
 
 	useEffect(() => {
